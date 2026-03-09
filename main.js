cfa1364ffdea1fede1f50e5b4f544a8eebecf4ea
brentcrude <brent161051@gmail.com>
2026-03-09 19:23:37 +0000

    Base PWD init

---
+++ main.js
@@ -0,0 +1,16 @@
+import * as constants from "./secret.js";
+import * as helpers from "./helpers.js";
+export let baseCorrect = false;
+const BaseButton = document.getElementById("BaseButton");
+
+BaseButton.addEventListener("click", () => {
+  const pwdInBase = document.getElementById("BaseCLR");  
+  if (helpers.checkPassword(pwdInBase, constants.BASECLRPWD)) {
+    alert("Access granted!");
+    baseCorrect = true;
+  } else {
+    alert("Wrong password!");
+    baseCorrect = false;
+  }
+});
+
